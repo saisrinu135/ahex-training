5587eaa769249d724451fd2de5d5116302837519
saisrinu <saisrinugampa135@gmail.com>
2024-09-12 09:56:38 +0530

    basic RAG done

--- langchain/RAG.py
+++ langchain/RAG.py
@@ -1,63 +1,103 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage, SystemMessage 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
 from dotenv import load_dotenv
 import os
 
+# load environment variables
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-3.5-turbo')
+# initialing a chat model
+model = ChatOpenAI(model='gpt-4')
+
+# output parser
+parser = StrOutputParser()
+
+# define the directories for current, file, and the database
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(curr_dir, 'pdfs', 'speed-control.pdf')
+persistant_dir = os.path.join(curr_dir, 'chroma_db')
 
 
-pdf = 'pdfs/speed-control.pdf'
-pdf_loader = PyPDFLoader(pdf)
-docs = pdf_loader.load()
+#Check if the vector database exists
+if not os.path.exists(persistant_dir):
+    print("\nDatabase not found. Creating one")
+
+    # checks if the file exixts
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"file {file_path} not found")
+    
+    # loading the document
+    print("\nLoading document")
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-splits = text_splitter.split_documents(docs)
+    # splitting the document into chunks
+    print("\nSplitting document into chunks")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    splits = text_splitter.split_documents(docs)
+    print(f"Document is splitted into {len(splits)} parts")
+
+    # Creating a vector store out of chunks
+    print("\nCreating a vector store")
+    vector_store = Chroma.from_documents(
+        documents=splits,
+        embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        persist_directory=persistant_dir
+    )
+    print("\nDatabase created")
+
+else:
+    print("\nDatabase found. Need not to initialize")
 
 
+#vectore store
+vector_store = Chroma(
+    persist_directory=persistant_dir,
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-small")
+    )
 
-vector_store = Chroma.from_documents(
-    documents=splits,
-    embedding=OpenAIEmbeddings(),
-    persist_directory="./chroma_db")
-
-retriever = vector_store.as_retriever(
+# initializing a retriever
+print("Creatig retriever")
+retreiver = vector_store.as_retriever(
     search_type='similarity',
     search_kwargs={'k': 2}
+    
 )
 
+# taking query
+query = "What is BLDC Motor? and How it is different from Traditional DC motor?"
 
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
-)
+# retriveing documents
+print("Retrieving related documents")
+documents = retreiver.invoke(query)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ('system', system_prompt),
-        ('human', "{input}")
-    ]
-)
+combined_input = f"""Here are the documents relatd to the query:
+                {query}
+                Relevant documents:
+                {"".join([doc.page_content for doc in documents])}
+                Please provide the answer based on the provided documents. If the answer is not there in documents reply with 'I don't know'
+                """
 
-question_answer_chain = create_stuff_documents_chain(model, prompt)
-retriever_chain = create_retrieval_chain(retriever, question_answer_chain)
+# creates chain
+chain = model | parser
 
-query = "What is the document about?"
-result = retriever_chain.invoke({'input':query})
-print(result)
+messages = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content=combined_input)
+]
+
+# invoking chain
+response = chain.invoke(messages)
+
+# printing the response
+print(response)
+
+
